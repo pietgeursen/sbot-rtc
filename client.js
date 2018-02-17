53fe26bf0a21e510d2a3dcb03ba0811d049ae42a
Piet Geursen <pietgeursen@gmail.com>
2018-02-17 13:23:31 +1300

    Gets basic signalling set up ok.

--- client.js
+++ client.js
@@ -1,44 +1,66 @@
 var signalhub = require('signalhub')
 var SimplePeer = require('simple-peer')
 var wrtc = require('wrtc')
+var cuid = require('cuid')
+var once = require('once');
 
 var hub = signalhub('my-app-name', [
   'localhost:9000'
 ])
 
 var peer1 = new SimplePeer({ initiator: true, wrtc })
+peer1.uuid = cuid()
 var peer2 = new SimplePeer({wrtc})
+peer2.uuid = cuid()
 
 peer1.on('error', console.log )
 peer2.on('error', console.log )
 
+//peer1 subscription to signalling data
 hub.subscribe('signal')
   .on('data', function(data) {
-    console.log(2);
-    peer2.signal(data)// 2
+    if(data.from !== peer1.uuid)
+      peer1.signal(data.data)
   })
 
 peer1.on('signal', function (data) { //this is kicked off immediately because it's the initiator
   // when peer1 has signaling data, give it to peer2 somehow
-  console.log(1, data);
-  hub.broadcast('signal', data)// 1
-})
-
-peer2.on('signal', function (data) {
-  // when peer2 has signaling data, give it to peer1 somehow
-  console.log(3);
-  peer1.signal(data) //3
+  var wrapped = Object.assign({}, {from: peer1.uuid}, {data})
+  hub.subscribe(peer1.uuid)
+    .once('data', function(data) {
+       peer1.signal(data) 
+    })
+  hub.broadcast('signal', wrapped)// 1
 })
 
 peer1.on('connect', function () {
   // wait for 'connect' event before using the data channel
-  console.log(4);
+  console.log('connect');
   peer1.send('hey peer2, how is it going?') //4
 })
 
+//////////////////////////////////////////////////////////////////////////
+
+//peer1 subscription to signalling data
+hub.subscribe('signal')
+  .on('data', function(data) {
+    if(data.from !== peer2.uuid)
+      peer2.signal(data.data)
+  })
+
+peer2.on('signal', function (data) {
+  var wrapped = Object.assign({}, {from: peer2.uuid}, {data})
+  hub.subscribe(peer2.uuid)
+    .once('data', function(data) {
+       peer2.signal(data) 
+    })
+  hub.broadcast('signal', wrapped)// 1
+
+})
+
+
 peer2.on('data', function (data) {
   // got a data channel message
-  console.log(5);
   console.log('got a message from peer1: ' + data)
 })
 

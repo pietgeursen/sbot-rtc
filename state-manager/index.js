ef643db0f5f598d5b16cec347ec74c2f7e68dbda
Piet Geursen <pietgeursen@gmail.com>
2018-02-28 13:44:10 +1300

    Wiring up without testing woooooooo.

--- state-manager/index.js
+++ state-manager/index.js
@@ -1,12 +1,12 @@
 'use strict'
 
-var fs = require('fs')
-var {start, pull} = require('inu')
-var pullAsync = require('pull-async')
-var Push = require('pull-pushable')
-var Cat = require('pull-cat')
-var wrtc = require('wrtc')
-var SignalHub = require('signalhub')
+const fs = require('fs')
+const {pull} = require('inu')
+const pullAsync = require('pull-async')
+const Push = require('pull-pushable')
+const Cat = require('pull-cat')
+const wrtc = require('wrtc')
+const SignalHub = require('signalhub')
 
 // Actions
 const REMOTE_PEER_DID_ANNOUNCE = Symbol('REMOTE_PEER_DID_ANNOUNCE')
@@ -18,6 +18,7 @@ const HUB_ADDRESS_ADDED = Symbol('HUB_ADDRESS_ADDED')
 const HUB_ADDRESS_REMOVED = Symbol('HUB_ADDRESS_REMOVED')
 const KNOWN_HUBS_ADDED = Symbol('KNOWN_HUBS_ADDED')
 const PEER_CONNECTION_TIMER_TICKED = Symbol('PEER_CONNECTION_TIMER_TICKED')
+const EMITTED_NEW_HUB = Symbol('EMITTED_NEW_HUB')
 
 // Effects
 const SCHEDULE_START_PEER_CONNECTION_TICK = Symbol('SCHEDULE_START_PEER_CONNECTION_TICK')
@@ -26,12 +27,27 @@ const SCHEDULE_SAVE_HUBS = Symbol('SCHEDULE_SAVE_HUBS')
 const SCHEDULE_CONNECT_TO_REMOTE_PEER = Symbol('SCHEDULE_CONNECT_TO_REMOTE_PEER')
 const SCHEDULE_BROADCAST_KNOWN_HUBS = Symbol('SCHEDULE_BROADCAST_KNOWN_HUBS')
 const SCHEDULE_ADD_KNOWN_HUBS = Symbol('SCHEDULE_ADD_KNOWN_HUBS')
+const SCHEDULE_SERVER_EMIT_NEW_HUB = Symbol('SCHEDULE_SERVER_EMIT_NEW_HUB')
 
 
 // Peer connection states
 const CONNECTION_STATE_CONNECTED = Symbol('CONNECTION_STATE_CONNECTED')
 const CONNECTION_STATE_DISCONNECTED = Symbol('CONNECTION_STATE_DISCONNECTED')
 const CONNECTION_STATE_CONNECTING = Symbol('CONNECTION_STATE_CONNECTING')
+
+function scheduleServerEmitNewHub ({hub}) {
+  return {
+    type: SCHEDULE_SERVER_EMIT_NEW_HUB,
+    hub
+  }
+}
+
+function emittedNewHub ({hub}) {
+  return {
+    type: EMITTED_NEW_HUB,
+    hub
+  }
+}
 
 function remotePeerDidAnnounce ({peer, hub}) {
   return {
@@ -53,10 +69,10 @@ function scheduleStartPeerConnectionTick () {
   }
 }
 
-function hubAddressAdded (address) {
+function hubAddressAdded ({hub}) {
   return {
     type: HUB_ADDRESS_ADDED,
-    address
+    hub
   }
 }
 
@@ -66,10 +82,11 @@ function knownHubsAdded () {
   }
 }
 
-function scheduleAnnounceToHub (address) {
+function scheduleAnnounceToHub ({hub, id}) {
   return {
     type: SCHEDULE_ANNOUNCE_TO_HUB,
-    address
+    hub,
+    id
   }
 }
 
@@ -79,10 +96,11 @@ function scheduleAddKnownHubs () {
   }
 }
 
-module.exports = function App ({Hub, pubKey, loadHubs}) {
+module.exports = function App ({Hub, pubKey, loadHubs, server}) {
   Hub = Hub || function Hub (hub) {
-    return SignalHub('sbot-rtc-gossip', 'https://' + hub) // TODO: version number
+    return SignalHub('sbot-rtc-gossip', hub) // TODO: version number
   }
+
   const loadHubsFromFile = ({filePath}, cb) => {
     filePath = filePath || './known_rtc_hubs.json'
     fs.readFile(filePath, cb)
@@ -107,24 +125,28 @@ module.exports = function App ({Hub, pubKey, loadHubs}) {
           return {model, effect: scheduleStartPeerConnectionTick()}
         }
         case HUB_ADDRESS_ADDED: {
-          if (model.hubs[action.address]) { return {model} }
+          if (model.hubs[action.hub]) { return {model} }
           const newHub = {
             connection: null,
             connectionAttempts: 0,
             peers: {}
           }
 
-          model.hubs[action.address] = newHub
-          return { model, effect: scheduleAnnounceToHub(action.address) }
+          const newModel = Object.assign({}, model, { hubs: {[action.hub]: newHub} })
+          return { model: newModel, effect: scheduleServerEmitNewHub({hub: action.hub}) }
+        }
+        case EMITTED_NEW_HUB: {
+          return { model, effect: scheduleAnnounceToHub({hub: action.hub, id: pubKey}) }
         }
         case REMOTE_PEER_DID_ANNOUNCE: {
-          if (model.peers[action.peer] || action.peer === pubKey) { return {model} }
+          if (model.hubs[action.hub].peers[action.peer] || action.peer === pubKey) { return {model} }
 
           const newPeer = {
             connectionsState: CONNECTION_STATE_DISCONNECTED
           }
-          model[action.hub][action.peer] = newPeer
-          return {model}
+          const newModel = Object.assign({}, model, { hubs: {[action.hub]: {peers: {[action.peer]: newPeer}}} })
+          console.log('new model', newModel.hubs[action.hub].peers)
+          return {model: newModel}
         }
         default:
           return {model}
@@ -133,7 +155,7 @@ module.exports = function App ({Hub, pubKey, loadHubs}) {
     run: function (effect, sources) {
       switch (effect.type) {
         case SCHEDULE_ANNOUNCE_TO_HUB: {
-          const hub = Hub(effect.address)
+          const hub = Hub(effect.hub)
           const p = Push()
 
           hub.broadcast('PEER_ANNOUNCE', {id: pubKey})
@@ -143,8 +165,14 @@ module.exports = function App ({Hub, pubKey, loadHubs}) {
 
           return pull(
             p,
-            pull.map((peer) => remotePeerDidAnnounce({peer, hub: effect.address}))
+            pull.map((peer) => remotePeerDidAnnounce({peer: peer.id, hub: effect.hub}))
           )
+        }
+        case SCHEDULE_SERVER_EMIT_NEW_HUB: {
+          if (server && server.emit) {
+            server.emit('RTC_HUB_ADDED', effect.hub)
+          }
+          return pull.once(emittedNewHub({hub: effect.hub}))
         }
         case SCHEDULE_ADD_KNOWN_HUBS: {
           return Cat([
@@ -152,11 +180,10 @@ module.exports = function App ({Hub, pubKey, loadHubs}) {
             pull(
               pullAsync((cb) => loadKnownHubs({}, cb)),
               pull.flatten(),
-              pull.map(hubAddressAdded)
+              pull.map(hub => hubAddressAdded({hub}))
             )
           ])
         }
-
         case SCHEDULE_START_PEER_CONNECTION_TICK: {
           const p = Push()
           setInterval(() => { p.push(peerConnectionTimerTicked()) }, 5e3)
--- test/state-manager/index.js
+++ test/state-manager/index.js
@@ -2,6 +2,7 @@ const test = require('tape')
 const {start} = require('inu')
 const App = require('../../state-manager/')
 const pull = require('pull-stream')
+const Hub = require('signalhub')
 
 test('hullo', function (t) {
   const loadHubs = (_, cb) => {
@@ -9,7 +10,12 @@ test('hullo', function (t) {
       'https://signalhub-jccqtwhdwc.now.sh'
     ])
   }
-  const {actions, effects, models} = start(App({loadHubs}))
+  const hub = Hub('sbot-rtc-gossip', 'https://signalhub-jccqtwhdwc.now.sh')
+  const {actions, effects, models} = start(App({loadHubs, pubKey: 'sdkjfkj39r9u=.nope'}))
+
+  setTimeout(() => {
+    hub.broadcast('PEER_ANNOUNCE', {id: 'piet'})
+  }, 3000)
 
   pull(
     models(),
